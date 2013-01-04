9cbcd9ba2fabee2285d476fb748c7cea483761ed
Andrew C. Dvorak <acdvorak@gmail.com>
2013-01-04 14:54:36 -0600

    Added Chapter.cs

---
+++ BDAutoMuxer/BDROM/Chapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BDAutoMuxer.BDInfo;
+
+namespace BDAutoMuxer.BDROM
+{
+    public class Chapter
+    {
+        public TimeSpan Start { get; private set; }
+        public Language Language;
+        public string Title;
+
+        public class Json
+        {
+            public TimeSpan start;
+            public string iso639_2;
+            public string title;
+
+            public Chapter FromJson()
+            {
+                return new Chapter {Start = start, Language = Language.GetLanguage(iso639_2), Title = title};
+            }
+        }
+    }
+}
