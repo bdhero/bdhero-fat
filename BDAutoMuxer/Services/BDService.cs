660cdab8c3e3996f0131bf762af661217aa44279
Andrew C. Dvorak <acdvorak@gmail.com>
2013-01-04 17:51:59 -0600

    Minor/ignore - Removed unused "using"

--- BDAutoMuxer/Services/BDService.cs
+++ BDAutoMuxer/Services/BDService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BDAutoMuxer.BDROM;
-using BDAutoMuxer.controllers;
 using Newtonsoft.Json;
 
 namespace BDAutoMuxer.Services
