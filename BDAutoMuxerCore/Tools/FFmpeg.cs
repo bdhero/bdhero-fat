27170e72a80c9b63e67f22ada986ea8f2aae6566
Andrew C. Dvorak <acdvorak@gmail.com>
2013-01-18 01:58:38 -0600

    Mostly working FFmpeg!
    Runs in background, parses progress information, and generates a correct output file.
    
    Progress file stream reader thread isn't functioning properly though...  It only prints out one line of progress to the Console.

--- BDAutoMuxerCore/Tools/FFmpeg.cs
+++ BDAutoMuxerCore/Tools/FFmpeg.cs
@@ -4,94 +4,151 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using BDAutoMuxerCore.BDROM;
+using DotNetUtils;
 using MediaInfoWrapper;
 using ProcessUtils;
 
 namespace BDAutoMuxerCore.Tools
 {
-    public class FFmpeg : AbstractExternalTool
+    public class FFmpeg : BackgroundProcessWorker
     {
         private const string FFmpegExeFilename = "ffmpeg.exe";
-        private readonly string _ffmpegExePath;
+
+        private static readonly Regex FrameRegex = new Regex(@"^frame=(\d+)$");
+        private static readonly Regex FpsRegex = new Regex(@"^fps=([\d.]+)$");
+        private static readonly Regex TotalSizeRegex = new Regex(@"^total_size=(\d+)$");
+        private static readonly Regex OutTimeMsRegex = new Regex(@"^out_time_ms=(\d+)$");
+        private static readonly Regex ProgressRegex = new Regex(@"^progress=(\w+)$");
+
+        private readonly string _progressFilePath;
+        private readonly TimeSpan _playlistLength;
         private readonly List<string> _inputM2TSPaths;
         private readonly List<Track> _selectedTracks;
         private readonly string _outputMKVPath;
 
-        public FFmpeg(List<string> inputM2TSPaths, List<Track> selectedTracks, string outputMKVPath)
-        {
-            _ffmpegExePath = Path.Combine(ToolTempDirPath, FFmpegExeFilename);
+        private long _curFrame;
+        private double _curFps;
+        private long _curSize;
+        private long _curOutTimeMs;
 
-            _inputM2TSPaths = inputM2TSPaths;
-            _selectedTracks = selectedTracks;
+        private readonly BackgroundWorker _progressWorker = new BackgroundWorker();
+
+        public FFmpeg(Playlist playlist, IEnumerable<Track> selectedTracks, string outputMKVPath)
+        {
+            _playlistLength = playlist.Length;
+            _inputM2TSPaths = playlist.StreamClips.Select(clip => clip.FileInfo.FullName).ToList();
+            _selectedTracks = selectedTracks.ToList();
             _outputMKVPath = outputMKVPath;
 
-            DoWork += MuxToMKV;
-        }
-
-        #region Muxing
-
-        public void MuxToMKV(object sender, DoWorkEventArgs doWorkEventArgs)
-        {
             if (_inputM2TSPaths.Count == 0)
                 throw new ArgumentOutOfRangeException("At least one input M2TS file is required.");
 
-            var input = _inputM2TSPaths.Count == 1 ? _inputM2TSPaths[0] : "concat:" + string.Join("|", _inputM2TSPaths);
-            var args = new ArgumentList();
+            _progressFilePath = Path.GetTempFileName();
 
-            args.AddAll("-i", input);
+            ExePath = AssemblyUtils.GetTempFilePath(GetType(), FFmpegExeFilename);
+
+            var inputFiles = _inputM2TSPaths.Count == 1 ? _inputM2TSPaths[0] : "concat:" + string.Join("|", _inputM2TSPaths);
+
+            // Replace existing files
+            Arguments.AddAll("-y");
+
+            // Send progress information to temp file
+            Arguments.AddAll("-progress", _progressFilePath);
+
+            Arguments.AddAll("-i", inputFiles);
 
             // Map selected tracks to output file
-            args.AddRange(_selectedTracks.SelectMany(track => new[] {"-map", "0:" + track.Index}));
+            Arguments.AddRange(_selectedTracks.SelectMany(track => new[] {"-map", "0:" + track.Index}));
 
             // Copy all codecs
-            args.AddAll("-c", "copy");
+            Arguments.AddAll("-c", "copy");
 
             // Convert Blu-ray LPCM tracks to signed, little endian PCM for MKV.
             // Blu-ray LPCM is signed, big endian, and either 16-, 20-, or 24-bit.
             // FFmpeg only outputs 16- or 24- bit PCM, so 20-bit Blu-ray LPCM needs to be converted to 24-bit PCM.
-            args.AddRange(_selectedTracks
+            Arguments.AddRange(_selectedTracks
                 .Where(track => track.Codec == MICodec.LPCM)
                 .SelectMany(track => new[] { "-c:a:" + track.IndexOfType, "pcm_s" + (track.BitDepth == 16 ? 16 : 24) + "le" }));
 
-            args.Add(_outputMKVPath);
+            Arguments.Add(_outputMKVPath);
 
-            Execute(args, sender, doWorkEventArgs);
+            ExtractResources();
+
+            BeforeStart += OnBeforeStart;
         }
 
-        #endregion
-
-        #region Abstract method implementation
-
-        protected override string Name
+        private void OnBeforeStart(object sender, EventArgs eventArgs)
         {
-            get { return "FFmpeg"; }
+            _progressWorker.DoWork += ProgressWorkerOnDoWork;
+            _progressWorker.RunWorkerAsync();
         }
 
-        protected override string Filename
+        private void ProgressWorkerOnDoWork(object sender, DoWorkEventArgs doWorkEventArgs)
         {
-            get { return FFmpegExeFilename; }
+            using (var stream = CreateProgressFileStream())
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        ParseProgressLine(reader.ReadLine());
+                    }
+                }
+            }
         }
 
-        protected override void ExtractResources()
+        private void ParseProgressLine(string line)
+        {
+            if (FrameRegex.IsMatch(line))
+                _curFrame = long.Parse(FrameRegex.Match(line).Groups[1].Value);
+            else if (FpsRegex.IsMatch(line))
+                _curFps = double.Parse(FpsRegex.Match(line).Groups[1].Value);
+            else if (TotalSizeRegex.IsMatch(line))
+                _curSize = long.Parse(TotalSizeRegex.Match(line).Groups[1].Value);
+            else if (OutTimeMsRegex.IsMatch(line))
+                _curOutTimeMs = long.Parse(OutTimeMsRegex.Match(line).Groups[1].Value) / 1000;
+
+            var prevProgress = _progress;
+
+            _progress = 100 * (_curOutTimeMs / _playlistLength.TotalMilliseconds);
+            _progress = Math.Min(_progress, 100);
+
+            if ("progress=end" == line)
+                _progress = 100;
+
+            if (prevProgress != _progress)
+                Console.WriteLine("{0}%", _progress.ToString("0.00"));
+        }
+
+        private FileStream CreateProgressFileStream()
+        {
+            return new FileStream(_progressFilePath,
+                                  FileMode.Open,
+                                  FileAccess.Read,
+                                  FileShare.ReadWrite | FileShare.Delete);
+        }
+
+        private void ExtractResources()
         {
             try
             {
-                File.WriteAllBytes(_ffmpegExePath, BinTools.ffmpeg);
+                File.WriteAllBytes(ExePath, BinTools.ffmpeg);
             }
-            catch {}
+            catch { }
         }
 
-        protected override void HandleOutputLine(string line, object sender, DoWorkEventArgs e)
+        public static void Test()
         {
-            throw new NotImplementedException();
+            var bdrom = new BDInfo.BDROM(@"Y:\BD\49123204_BLACK_HAWK_DOWN");
+            bdrom.Scan();
+            var disc = Disc.Transform(bdrom);
+            var playlist = disc.Playlists.FirstOrDefault(playlist1 => playlist1.Filename.StartsWith("00039"));
+            var selectedTracks = playlist.Tracks;
+            var outputMKVPath = @"Y:\BDAM\out\progress\BLACK_HAWK_DOWN_00039.mpls.mkv";
+            var ffmpeg = new FFmpeg(playlist, selectedTracks, outputMKVPath);
+            ffmpeg.StartAsync();
         }
-
-        protected override ISet<string> GetOutputFilesImpl()
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
     }
 }
