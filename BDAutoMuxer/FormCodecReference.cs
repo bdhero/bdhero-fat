68394a2b02b536e0617d03bd1bae1e7ef831942f
Andrew C. Dvorak <acdvorak@gmail.com>
2013-05-18 16:38:39 -0500

    Renamed BDHero project back to BDAutoMuxer (will create a fresh, new BDHero project in a later commit)

---
+++ BDAutoMuxer/FormCodecReference.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BDAutoMuxer.Properties;
+using BDAutoMuxer.Views;
+using DotNetUtils;
+using MediaInfoWrapper;
+
+namespace BDAutoMuxer
+{
+    public partial class FormCodecReference : Form
+    {
+        private static FormCodecReference _instance;
+
+        private readonly ImageList _icons = new ImageList {ColorDepth = ColorDepth.Depth32Bit};
+
+        public static void ShowReference(MICodec autoSelectCodec = null)
+        {
+            if (_instance == null)
+            {
+                _instance = new FormCodecReference(autoSelectCodec);
+                _instance.Disposed += (sender, eventArgs) => _instance = null;
+                _instance.Show();
+            }
+            else
+            {
+                _instance.Focus();
+                _instance.SelectCodec(autoSelectCodec);
+            }
+        }
+
+        private FormCodecReference(MICodec autoSelectCodec = null)
+        {
+            InitializeComponent();
+
+            listViewCodecs.Items.Clear();
+
+            AddCodecs(0, MICodec.VideoCodecs.Select(c => c as MICodec).ToList());
+            AddCodecs(1, MICodec.AudioCodecs.Select(c => c as MICodec).ToList());
+            AddCodecs(2, MICodec.SubtitleCodecs.Select(c => c as MICodec).ToList());
+
+            columnHeaderCommonName.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            columnHeaderOfficialName.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            columnHeaderAlternateNames.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            columnHeaderCodecId.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+            FormUtils.TextBox_EnableSelectAll(this);
+
+            listViewCodecs.Items[0].Selected = true;
+
+            Load += (sender, args) => SelectCodec(autoSelectCodec);
+        }
+
+        public void SelectCodec(MICodec codec = null)
+        {
+            if (CanFocus) Focus();
+            if (codec != null && codec.IsKnown)
+            {
+                var listViewItem = listViewCodecs.Items.OfType<ListViewItem>().FirstOrDefault(item => CodecMatches(item, codec));
+                if (listViewItem != null)
+                {
+                    listViewItem.Selected = true;
+                    listViewItem.Focused = true;
+                }
+            }
+        }
+
+        private static bool CodecMatches(ListViewItem item, MICodec codec)
+        {
+            return (item.Tag as MICodec) == codec;
+        }
+
+        private void AddCodecs(int groupIndex, IEnumerable<MICodec> codecs)
+        {
+            foreach (var codec in codecs)
+            {
+                var videoCodec = codec as MIVideoCodec;
+                var audioCodec = codec as MIAudioCodec;
+
+                var core = audioCodec != null && audioCodec.Core != null ? audioCodec.Core.CommonName : null;
+                var compression = audioCodec != null ? (audioCodec.Lossless ? "Lossless" : "Lossy") : (codec.IsVideo ? "Lossy" : "N/A");
+                var altDisplayNames = string.Join(",  ", new HashSet<string>(codec.AltDisplayNames));
+                var isMuxable = codec.IsMuxable ? "yes" : "NO";
+
+                var icon = Resources.type_subtitle;
+
+                if (codec.IsVideo) icon = Resources.type_video;
+                if (codec.IsAudio) icon = Resources.type_audio;
+
+                _icons.Images.Add(icon);
+
+                var subitems = new[]
+                                   {
+                                       new ListViewItem.ListViewSubItem { Text = codec.CommonName },
+                                       new ListViewItem.ListViewSubItem { Text = codec.FullName },
+                                       new ListViewItem.ListViewSubItem { Text = altDisplayNames },
+                                       new ListViewItem.ListViewSubItem { Text = codec.CodecId },
+                                       new ListViewItem.ListViewSubItem { Text = core },
+                                       new ListViewItem.ListViewSubItem { Text = compression },
+                                       new ListViewItem.ListViewSubItem { Text = isMuxable }
+                                   };
+                // Variable `i` needs to be outside of ListViewItem() initializer to avoid closure craziness (lazy evaluation)
+                var i = listViewCodecs.Items.Count;
+                var listViewItem = new ListViewItem(subitems, 0)
+                                       {
+                                           Tag = codec,
+                                           ImageIndex = i,
+                                           Group = listViewCodecs.Groups[groupIndex]
+                                       };
+                listViewCodecs.Items.Add(listViewItem);
+            }
+            listViewCodecs.SmallImageList = _icons;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void listViewCodecs_SelectedIndexChanged(object sender = null, EventArgs e = null)
+        {
+            textBoxDescription.Text = null;
+            labelOfficialBlurayValue.Text = "N/A";
+            labelOfficialDVDValue.Text = "N/A";
+            pictureBoxLogo.Image = null;
+
+            var item = listViewCodecs.SelectedItems.OfType<ListViewItem>().FirstOrDefault();
+            if (item == null) return;
+
+            var codec = item.Tag as MICodec;
+            if (codec == null) return;
+
+            textBoxDescription.Text = codec.Description;
+            labelOfficialBlurayValue.Text = codec.IsOfficialBlurayCodec
+                                                ? string.Format("Yes ({0})",
+                                                                codec.IsRequiredBlurayCodec ? "required" : "optional")
+                                                : "No";
+            labelOfficialDVDValue.Text = codec.IsOfficialDVDCodec
+                                             ? string.Format("Yes ({0})",
+                                                             codec.IsRequiredDVDCodec ? "required" : "optional")
+                                             : "No";
+            pictureBoxLogo.Image = codec.Logo;
+        }
+    }
+}
---
+++ BDAutoMuxer/FormCoverArt.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using BDHeroCore.Services;
+using DotNetUtils;
+
+namespace BDAutoMuxer
+{
+    public partial class FormCoverArt : Form
+    {
+        private readonly ImageList coverArtImages = new ImageList { ColorDepth = ColorDepth.Depth32Bit, ImageSize = new Size(93, 139) };
+            
+        public int SelectedIndex { get; private set;}
+
+        public FormCoverArt(List<string> posterList, int index, string language)
+        {
+            InitializeComponent();
+            PopulateCoverArt(posterList, index);
+            AcceptButton.DialogResult = DialogResult.OK;
+
+            if (coverArtImages != null)
+            {
+                try
+                {
+                    Icon = Icon.FromHandle(((Bitmap)coverArtImages.Images[index]).GetHicon());
+                }
+                catch {}
+            }
+
+            if (string.IsNullOrEmpty(language))
+            {
+                Text = "Posters";
+            }
+            else
+            {
+                try
+                {
+                    var lang = new CultureInfo(language).DisplayName;
+                    Text = lang + " Posters";
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public override sealed string Text
+        {
+            get { return base.Text; }
+            set { base.Text = value; }
+        }
+
+        #region Private Methods
+
+        private void PopulateCoverArt(List<string> posterList, int index)
+        {
+            for (var i = 0; i < posterList.Count; i++)
+            {
+                var posterUrl = posterList[i];
+                var listViewItem = new ListViewItem("", i);
+
+                listViewCovers.Items.Add(listViewItem);
+                coverArtImages.Images.Add(HttpRequest.GetImage(posterUrl));
+            }
+
+            listViewCovers.LargeImageList = coverArtImages;
+            listViewCovers.Items[index].Selected = true;
+
+        }
+      
+        #endregion
+        
+        #region Event Handlers
+
+        private void listViewCovers_DoubleClick(object sender, EventArgs e)
+        {
+            buttonSave.PerformClick();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (listViewCovers.SelectedIndices.Count > 0)
+            {
+                SelectedIndex = listViewCovers.SelectedIndices[0];
+            }
+            Close();
+        }
+
+        #endregion
+    }
+}
