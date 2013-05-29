834281eca2ab77a89eb98b2c385cfb893b0ec3bc
Andrew C. Dvorak <acdvorak@gmail.com>
2013-05-28 23:56:59 -0500

    Renamed ChapterWriterMuxerPlugin to ChapterWriterPlugin

--- ChapterWriterPlugin/Properties/AssemblyInfo.cs
+++ ChapterWriterPlugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChapterWriterMuxerPlugin")]
+[assembly: AssemblyTitle("ChapterWriterPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChapterWriterMuxerPlugin")]
+[assembly: AssemblyProduct("ChapterWriterPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
