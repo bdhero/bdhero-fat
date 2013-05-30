752e368ac6eb7cb3a82ca9f6783e381b7bb22aa4
Andrew C. Dvorak <acdvorak@gmail.com>
2013-05-30 14:29:22 -0500

    Added MkvMergePlugin.cs

---
+++ MKVMergeMuxerPlugin/MkvMergePlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BDHero.Queue;
+
+namespace BDHero.Plugin.MKVMergeMuxer
+{
+    public class MkvMergePlugin : IMuxerPlugin
+    {
+        public IPluginHost Host { get; set; }
+        public string Name { get { return "mkvmerge (mkvtoolnix)"; } }
+
+        public event PluginProgressHandler ProgressUpdated;
+        public event EditPluginPreferenceHandler EditPreferences;
+
+        public MatroskaFeatures MatroskaFeatures
+        {
+            get
+            {
+                return MatroskaFeatures.Chapters
+                     | MatroskaFeatures.CoverArt
+                     | MatroskaFeatures.DefaultFlag
+                     | MatroskaFeatures.ForcedFlag
+                    ;
+            }
+        }
+
+        public void LoadPlugin()
+        {
+        }
+
+        public void UnloadPlugin()
+        {
+        }
+
+        public void Mux(Job job)
+        {
+        }
+    }
+}
