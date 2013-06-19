0e5b2caff724d386e5c99e37420b7d4fcd34f4e8
Andrew C. Dvorak <acdvorak@gmail.com>
2013-06-18 21:42:37 -0500

    Added command line options to BDHeroCLI project using Mono.Options library

--- BDHeroCLI/Program.cs
+++ BDHeroCLI/Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using BDHero;
 using BDHero.Plugin;
+using DotNetUtils;
+using Mono.Options;
+using ProcessUtils;
 
 namespace BDHeroCLI
 {
@@ -23,23 +26,29 @@ namespace BDHeroCLI
 
         static void Main(string[] args)
         {
-            Controller.JobBeforeStart += ControllerOnJobBeforeStart;
-            Controller.JobSucceeded += ControllerOnJobSucceeded;
-            Controller.PluginProgressUpdated += ControllerOnPluginProgressUpdated;
-            Controller.LoadPlugins();
-
             string bdromPath = "",
                    mkvPath = "";
+            bool? replace;
 
-            if (args.Length == 2)
+            var optionSet = new OptionSet
+                {
+                    { "h|?|help", v => ShowHelp() },
+                    { "version", s => ShowVersion() },
+                    { "v|verbose", s => Logger.Logger.Repository.Threshold = log4net.Core.Level.Debug },
+                    { "w|warn", s => Logger.Logger.Repository.Threshold = log4net.Core.Level.Warn },
+                    { "q|quiet", s => Logger.Logger.Repository.Threshold = log4net.Core.Level.Error },
+                    { "s|silent", s => Logger.Logger.Repository.Threshold = log4net.Core.Level.Fatal },
+                    { "i=|input=", s => bdromPath = s },
+                    { "o=|output=", s => mkvPath = s },
+                    { "y", s => replace = true },
+                    { "n", s => replace = false }
+                };
+
+            var extraArgs = optionSet.Parse(args);
+
+            if (extraArgs.Any())
             {
-                bdromPath = args[0];
-                mkvPath = args[1];
-            }
-            else if (args.Length != 0)
-            {
-                Console.Error.WriteLine("Usage: {0} BDROM_PATH OUTPUT_MKV_PATH");
-                Environment.Exit(1);
+                Logger.WarnFormat("Unknown argument{0}: {1}", extraArgs.Count == 1 ? "" : "s", new ArgumentList(extraArgs));
             }
 
             // Prompt user for path to BD-ROM directory
@@ -55,6 +64,11 @@ namespace BDHeroCLI
                 Console.Write("Output MKV path: ");
                 mkvPath = Console.ReadLine();
             }
+
+            Controller.JobBeforeStart += ControllerOnJobBeforeStart;
+            Controller.JobSucceeded += ControllerOnJobSucceeded;
+            Controller.PluginProgressUpdated += ControllerOnPluginProgressUpdated;
+            Controller.LoadPlugins();
 
             if (Controller.Scan(bdromPath, mkvPath))
             {
@@ -79,6 +93,18 @@ namespace BDHeroCLI
             Console.WriteLine("*** BDhero CLI Finished - press <ENTER> to exit ***");
             Console.WriteLine();
             Console.Read();
+        }
+
+        private static void ShowHelp(int exitCode = 0)
+        {
+            Console.Error.WriteLine("Usage: {0} -i BDROM_PATH -o OUTPUT_MKV_PATH", Path.GetFileName(AssemblyUtils.AssemblyOrDefault().Location));
+            Environment.Exit(exitCode);
+        }
+
+        private static void ShowVersion(int exitCode = 0)
+        {
+            Console.Error.WriteLine("{0} v{1} - compiled {2}", AssemblyUtils.GetAssemblyName(), AssemblyUtils.GetAssemblyVersion(), AssemblyUtils.GetLinkerTimestamp());
+            Environment.Exit(exitCode);
         }
 
         private static void ControllerOnJobBeforeStart(object sender, EventArgs eventArgs)
