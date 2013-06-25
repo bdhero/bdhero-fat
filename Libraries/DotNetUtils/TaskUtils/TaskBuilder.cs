ec6ef2dc2e63fae33ea6c579969380c832accbe5
Andrew C. Dvorak <acdvorak@gmail.com>
2013-06-25 12:03:04 -0500

    Added TaskUtils namespace to DotNetUtils project w/ TaskBuilder and ThreadInvoker classes

---
+++ Libraries/DotNetUtils/TaskUtils/TaskBuilder.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetUtils.TaskUtils
+{
+    public class TaskBuilder
+    {
+        private TaskScheduler _callbackThread;
+        private CancellationToken _cancellationToken;
+        private IThreadInvoker _invoker;
+
+        private Action<CancellationToken> _beforeStart;
+        private Action<IThreadInvoker, CancellationToken> _work;
+        private Action<CancellationToken> _succeed;
+        private Action<CancellationToken, Exception> _fail;
+        private Action<CancellationToken> _finally;
+
+        public TaskBuilder OnThread(TaskScheduler callbackThread)
+        {
+            _callbackThread = callbackThread;
+            return this;
+        }
+
+        public TaskBuilder OnCurrentThread()
+        {
+            // Get the calling thread's context
+            _callbackThread = (SynchronizationContext.Current != null
+                                  ? TaskScheduler.FromCurrentSynchronizationContext()
+                                  : TaskScheduler.Default);
+            return this;
+        }
+
+        public TaskBuilder CancelWith(CancellationToken cancellationToken)
+        {
+            _cancellationToken = cancellationToken;
+            return this;
+        }
+
+        /// <summary>
+        /// Runs the specified action in the UI thread before invoking the main <see cref="DoWork"/> action.
+        /// </summary>
+        /// <param name="beforeStart"></param>
+        /// <returns>Reference to this <code>TaskBuilder</code></returns>
+        public TaskBuilder BeforeStart(Action<CancellationToken> beforeStart)
+        {
+            _beforeStart = beforeStart;
+            return this;
+        }
+
+        /// <summary>
+        /// Runs in background (non-UI) thread, but may execute an action on the UI thread by calling the passed thread invoker.
+        /// </summary>
+        /// <param name="work">Action to invoke on the background thread</param>
+        /// <returns>Reference to this <code>TaskBuilder</code></returns>
+        public TaskBuilder DoWork(Action<IThreadInvoker, CancellationToken> work)
+        {
+            _work = work;
+            return this;
+        }
+
+        /// <summary>
+        /// Runs the specified action in the UI thread after the main <see cref="DoWork"/> action runs
+        /// and completes successfully.
+        /// </summary>
+        /// <param name="succeed">Action to invoke on the UI thread when the main DoWork action completes successfully</param>
+        /// <returns>Reference to this <code>TaskBuilder</code></returns>
+        public TaskBuilder Succeed(Action<CancellationToken> succeed)
+        {
+            _succeed = succeed;
+            return this;
+        }
+
+        /// <summary>
+        /// Runs the specified action in the UI thread after the main <see cref="DoWork"/> action runs
+        /// and fails (due to an exception, etc.).
+        /// </summary>
+        /// <param name="fail">Action to invoke on the UI thread when the main DoWork action fails</param>
+        /// <returns>Reference to this <code>TaskBuilder</code></returns>
+        public TaskBuilder Fail(Action<CancellationToken, Exception> fail)
+        {
+            _fail = fail;
+            return this;
+        }
+
+        /// <summary>
+        /// Runs the specified action in the UI thread after the main <see cref="DoWork"/> action runs
+        /// and after the Success or Failure callbacks have ran, regardless of whether the main DoWork action failed or succeeded.
+        /// </summary>
+        /// <param name="finally"></param>
+        /// <returns>Reference to this <code>TaskBuilder</code></returns>
+        public TaskBuilder Finally(Action<CancellationToken> @finally)
+        {
+            _finally = @finally;
+            return this;
+        }
+
+        public Task Build()
+        {
+            var scheduler = _callbackThread;
+            var token = _cancellationToken;
+
+            if (scheduler == null)
+            {
+                throw new InvalidOperationException("Missing TaskScheduler");
+            }
+
+            if (token == null)
+            {
+                throw new InvalidOperationException("Missing CancellationToken");
+            }
+
+            _invoker = new ThreadInvoker(scheduler, token);
+
+            var stageTask = new Task(delegate
+                {
+                    try
+                    {
+                        if (InvokeBeforeStart() && InvokeDoWork())
+                        {
+                            InvokeSucceed();
+                        }
+                    }
+                    finally
+                    {
+                        InvokeFinally();
+                    }
+                });
+
+            return stageTask;
+        }
+
+        private bool InvokeBeforeStart()
+        {
+            if (_beforeStart == null) return true;
+            return Try(() => _invoker.InvokeOnUIThreadSync(_beforeStart));
+        }
+
+        private bool InvokeDoWork()
+        {
+            Debug.Assert(_work != null);
+            return Try(() => _work(_invoker, _cancellationToken));
+        }
+
+        private void InvokeSucceed()
+        {
+            if (_succeed == null) return;
+            Try(() => _invoker.InvokeOnUIThreadSync(_succeed));
+        }
+
+        private void InvokeFinally()
+        {
+            if (_finally == null) return;
+            Try(() => _invoker.InvokeOnUIThreadSync(_finally));
+        }
+
+        private bool Try(Action action)
+        {
+            if (_fail != null)
+            {
+                try
+                {
+                    action();
+                }
+                catch (Exception exception)
+                {
+                    _fail(_cancellationToken, exception);
+                    return false;
+                }
+            }
+            else
+            {
+                action();
+            }
+            return true;
+        }
+    }
+}
---
+++ Libraries/DotNetUtils/TaskUtils/ThreadInvoker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetUtils.TaskUtils
+{
+    /// <summary>
+    /// Concrete implementation of <code>IThreadInvoker</code>.
+    /// </summary>
+    public class ThreadInvoker : IThreadInvoker
+    {
+        private readonly TaskScheduler _callbackThread;
+
+        private readonly CancellationToken _cancellationToken;
+
+        /// <summary>
+        /// Constructs a thread invoker that executes actions in the context of the specified task scheduler, passing them 
+        /// </summary>
+        /// <param name="callbackThread"></param>
+        /// <param name="cancellationToken"></param>
+        public ThreadInvoker(TaskScheduler callbackThread, CancellationToken cancellationToken)
+        {
+            _callbackThread = callbackThread;
+            _cancellationToken = cancellationToken;
+        }
+
+        public void InvokeOnUIThreadSync(Action<CancellationToken> action)
+        {
+            StartTask(action, _cancellationToken).Wait();
+        }
+
+        public void InvokeOnUIThreadAsync(Action<CancellationToken> action)
+        {
+            StartTask(action, _cancellationToken);
+        }
+
+        private Task StartTask(Action<CancellationToken> action, CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(() => action(cancellationToken), _cancellationToken, TaskCreationOptions.None, _callbackThread);
+        }
+    }
+}
