82235bc3dc131e67d7ffc2ba6643e0ed19de35ce
Andrew C. Dvorak <acdvorak@gmail.com>
2013-06-26 22:40:13 -0500

    Updated BDHeroGUI assembly info

--- BDHeroGUI/Properties/AssemblyInfo.cs
+++ BDHeroGUI/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BDHeroGUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BDHero Graphical Interface")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BDHeroGUI")]
+[assembly: AssemblyProduct("BDHero")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
