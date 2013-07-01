5299a2f5affa92bc0de57dcc925ace71e9422287
Andrew C. Dvorak <acdvorak@gmail.com>
2013-06-30 22:20:52 -0500

    Added DoubleBufferedListView.cs to DotNetUtils project

---
+++ Libraries/DotNetUtils/Controls/DoubleBufferedListView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DotNetUtils.Controls
+{
+    public class DoubleBufferedListView : ListView
+    {
+        public DoubleBufferedListView()
+        {
+            DoubleBuffered = true;
+        }
+
+        protected override sealed bool DoubleBuffered
+        {
+            get { return base.DoubleBuffered; }
+            set { base.DoubleBuffered = value; }
+        }
+    }
+}
