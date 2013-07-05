2886565a2ecfc1975e23b52178f9781d8ef25299
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-05 11:59:54 -0500

    Added BDFileUtils.cs to BDHero project

---
+++ BDHero/Utils/BDFileUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DotNetUtils;
+using DotNetUtils.Annotations;
+
+namespace BDHero.Utils
+{
+    public static class BDFileUtils
+    {
+        [CanBeNull]
+        public static string GetBDROMDirectory([CanBeNull] string path)
+        {
+            while (!string.IsNullOrWhiteSpace(path))
+            {
+                if (File.Exists(Path.Combine(path, "BDMV", "index.bdmv")))
+                    return path;
+                path = Path.GetDirectoryName(path);
+            }
+
+            return null;
+        }
+
+        public static bool ContainsBDROMDirectory([CanBeNull] string path)
+        {
+            return GetBDROMDirectory(path) != null;
+        }
+    }
+}
