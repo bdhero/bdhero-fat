8765e2e7e82ef109c6fa7c4445a4147e916d4a02
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-06 19:56:17 -0500

    Added bdhero_gui_128.png to GUI project

--- BDHeroGUI/Properties/Resources.Designer.cs
+++ BDHeroGUI/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BDHeroGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bdhero_gui_128 {
+            get {
+                object obj = ResourceManager.GetObject("bdhero_gui_128", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap no_poster_w185 {
             get {
                 object obj = ResourceManager.GetObject("no_poster_w185", resourceCulture);
