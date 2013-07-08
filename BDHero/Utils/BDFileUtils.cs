549baf56d7ba4d0064673e8db73f8db8c337f1a5
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-07 21:08:15 -0500

    Added BDFileUtils.IsBDROM(DriveInfo drive)

--- BDHero/Utils/BDFileUtils.cs
+++ BDHero/Utils/BDFileUtils.cs
@@ -23,6 +23,11 @@ namespace BDHero.Utils
             return null;
         }
 
+        public static bool IsBDROM(DriveInfo drive)
+        {
+            return IsBDROM(drive.Name);
+        }
+
         public static bool IsBDROM([CanBeNull] string path)
         {
             return GetBDROMDirectory(path) != null;
