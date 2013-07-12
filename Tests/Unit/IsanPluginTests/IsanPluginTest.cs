72b740ccad874c664d6e95c8331e92d52054b5c1
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-12 15:31:25 -0500

    Added a few ISAN parsing unit tests to IsanPluginTest.cs

--- Tests/Unit/IsanPluginTests/IsanPluginTest.cs
+++ Tests/Unit/IsanPluginTests/IsanPluginTest.cs
@@ -22,7 +22,46 @@ namespace IsanPluginTests
         }
 
         [Test]
-        public void TestEmptyTitle()
+        public void TestParseCheckedIsanWithNumericCheckDigits()
+        {
+            const string checkedNumber = "00000000E06600003000000009";
+
+            var isan = Isan.TryParse(checkedNumber);
+
+            Assert.IsNotNull(isan);
+            Assert.AreEqual(isan.Root, "00000000E066");
+            Assert.AreEqual(isan.Episode, "0000");
+            Assert.AreEqual(isan.Version, "00000000");
+        }
+
+        [Test]
+        public void TestParseCheckedIsanWithAlphaCheckDigits()
+        {
+            const string checkedNumber = "00000000E0660000A00000000S";
+
+            var isan = Isan.TryParse(checkedNumber);
+
+            Assert.IsNotNull(isan);
+            Assert.AreEqual(isan.Root, "00000000E066");
+            Assert.AreEqual(isan.Episode, "0000");
+            Assert.AreEqual(isan.Version, "00000000");
+        }
+
+        [Test]
+        public void TestParseUncheckedIsan()
+        {
+            const string uncheckedNumber = "00000000E0AA000000000001";
+
+            var isan = Isan.TryParse(uncheckedNumber);
+
+            Assert.IsNotNull(isan);
+            Assert.AreEqual(isan.Root, "00000000E0AA");
+            Assert.AreEqual(isan.Episode, "0000");
+            Assert.AreEqual(isan.Version, "00000001");
+        }
+
+        [Test]
+        public void TestPopulateEmptyTitle()
         {
             const string emptyTitleVIsan = "00000000E0AA000000000001";
 
