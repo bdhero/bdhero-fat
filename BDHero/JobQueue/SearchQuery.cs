7a177b96b87f7d10e6a988566f68befc267a081f
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-12 12:07:45 -0500

    Added JobQueue/SearchQuery.cs to BDHero project

---
+++ BDHero/JobQueue/SearchQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDHero.JobQueue
+{
+    public class SearchQuery
+    {
+        public string Title;
+        public int? Year;
+
+        public override string ToString()
+        {
+            return Year.HasValue
+                       ? string.Format("{0} ({1})", Title, Year)
+                       : Title;
+        }
+    }
+}
