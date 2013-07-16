c7186501ff3a22eb6bcb6effa2638f9132091b59
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-16 10:23:35 -0500

    Updated chapter name regex to catch invalid chapter names for "Super 8" in ChapterGrabberPlugin.cs

--- Plugins/ChapterGrabberPlugin/ChapterGrabberPlugin.cs
+++ Plugins/ChapterGrabberPlugin/ChapterGrabberPlugin.cs
@@ -222,7 +222,7 @@ namespace ChapterGrabberPlugin
             TimeSpan parsed;
             if (TimeSpan.TryParse(trimmed, out parsed))
                 return true;
-            if (Regex.IsMatch(trimmed, @"^(?:Chapter|Scene|Kapitel)?\s*[0-9]+\s*$", RegexOptions.IgnoreCase))
+            if (Regex.IsMatch(trimmed, @"^(?:Chapter|Scene|Kapitel)?[\W_]*[0-9]+\s*$", RegexOptions.IgnoreCase))
                 return true;
             return false;
         }
