0fe9c3f4d0362e447810ddb2fc7d29365723372e
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-18 00:01:53 -0500

    Added play_blue.png to BDHeroGUI

--- BDHeroGUI/Properties/Resources.Designer.cs
+++ BDHeroGUI/Properties/Resources.Designer.cs
@@ -223,6 +223,16 @@ namespace BDHeroGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap play_blue {
+            get {
+                object obj = ResourceManager.GetObject("play_blue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap refresh_green {
             get {
                 object obj = ResourceManager.GetObject("refresh_green", resourceCulture);
