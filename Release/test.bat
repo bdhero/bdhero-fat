d039f152d4b6cf8d221062b9134e0ee1d309bfc2
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-19 17:57:53 -0500

    Removed double quotes from %nunit_home% in test.bat

--- Release/test.bat
+++ Release/test.bat
@@ -7,4 +7,4 @@ echo nunit_home=%nunit_home%
 set test1=Tests\Unit\DotNetUtilsUnitTests\bin\Debug\DotNetUtilsUnitTests.dll
 set test2=Tests\Unit\IsanPluginTests\bin\Debug\IsanPluginTests.dll
 
-"%nunit_home%\bin\nunit-console.exe" "%test1%" "%test2%" /xml=nunit-result.xml
+%nunit_home%\bin\nunit-console.exe "%test1%" "%test2%" /xml=nunit-result.xml
