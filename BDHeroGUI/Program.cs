e1f7d5c1c4698b14e1a5aa0a77aefc1bb5944c43
Andrew C. Dvorak <acdvorak@gmail.com>
2013-07-23 10:46:37 -0500

    Restored call to JobObjectController.BreakCurrentProcessOutOfPCAJobObject(args) in GUI/Program.cs

--- BDHeroGUI/Program.cs
+++ BDHeroGUI/Program.cs
@@ -17,8 +17,11 @@ namespace BDHeroGUI
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (JobObjectController.BreakCurrentProcessOutOfPCAJobObject(args))
+                return;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(CreateMainForm());
