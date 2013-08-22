fefb7b277ab6c0bd8f176dc99915811cd0ba613a
Andrew C. Dvorak <acdvorak@gmail.com>
2013-08-22 04:51:55 -0500

    Use InnoSetup to create Windows installer and create 7-Zip, SFX, and Zip archives in package.bat

--- Release/package.bat
+++ Release/package.bat
@@ -2,4 +2,16 @@
 
 REM %CD% = C:\Projects\BDHero
 
-"%ProgramFiles%\BitRock InstallBuilder Enterprise 8.6.0\bin\builder-cli.exe" build "Installer.xml" windows
+REM "%ProgramFiles%\BitRock InstallBuilder Enterprise 8.6.0\bin\builder-cli.exe" build "Installer.xml" windows
+
+iscc "/sCustom=signtool.exe $p" Installer\InnoSetup\setup.iss
+
+Release\Versioner.exe -v > VERSION.tmp
+set /p Version= < VERSION.tmp
+del VERSION.tmp
+
+7za a -sfx7z.sfx -r Artifacts\bdhero-%Version%-sfx.exe Artifacts\Portable\*
+7za a -r Artifacts\bdhero-%Version%.7z Artifacts\Portable\*
+7za a -r Artifacts\bdhero-%Version%.zip Artifacts\Portable\*
+
+Release\sign.bat "BDHero Portable (Self Extracting Archive)" "http://bdhero.org/" Artifacts\bdhero-%Version%-sfx.exe
