f29336620d4ec67a9121579f525da8fc19b4c818
Andrew C. Dvorak <acdvorak@gmail.com>
2013-08-22 19:18:50 -0500

    Minor - Added space between number and suffix when formatting zero bytes in FileUtils

--- Libraries/DotNetUtils/FileUtils.cs
+++ Libraries/DotNetUtils/FileUtils.cs
@@ -140,7 +140,7 @@ namespace DotNetUtils
         {
             string[] suf = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" }; // Longs run out around EB
             if (byteCount == 0)
-                return "0" + suf[0];
+                return "0 " + suf[0];
             long bytes = Math.Abs(byteCount);
             int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
             double num = Math.Round(bytes / Math.Pow(1024, place), 1);
