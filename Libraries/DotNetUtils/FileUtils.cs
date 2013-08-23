ee7a7b4a5853c04d92b289fa3cab122f4ad77694
Andrew C. Dvorak <acdvorak@gmail.com>
2013-08-22 19:19:28 -0500

    Don't display fractional values for bytes in FileUtils.BytesToString()

--- Libraries/DotNetUtils/FileUtils.cs
+++ Libraries/DotNetUtils/FileUtils.cs
@@ -144,7 +144,8 @@ namespace DotNetUtils
             long bytes = Math.Abs(byteCount);
             int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
             double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return string.Format("{0} {1}", (Math.Sign(byteCount) * num).ToString("0.0"), suf[place]);
+            string fmt = place == 0 ? "0" : "0.0";
+            return string.Format("{0} {1}", (Math.Sign(byteCount) * num).ToString(fmt), suf[place]);
         }
 
         /// <summary>
