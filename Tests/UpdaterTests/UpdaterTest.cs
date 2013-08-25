f9ef9c966fda6f34e8d8a37b20ce7b5884c78598
Andrew C. Dvorak <acdvorak@gmail.com>
2013-08-25 15:28:11 -0500

    Updated Updater unit tests to timeout after 15 seconds

--- Tests/UpdaterTests/UpdaterTest.cs
+++ Tests/UpdaterTests/UpdaterTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using NUnit.Framework;
 using Updater;
@@ -10,27 +11,48 @@ namespace UpdaterTests
     [TestFixture]
     public class UpdaterTest
     {
+        private UpdaterClient _client;
+
+        [SetUp]
+        public void SetUpClient()
+        {
+            _client = new UpdaterClient();
+            _client.BeforeRequest += ClientOnBeforeRequest;
+        }
+
+        [TearDown]
+        public void TearDownClient()
+        {
+            if (_client != null)
+                _client.BeforeRequest -= ClientOnBeforeRequest;
+        }
+
+        private void ClientOnBeforeRequest(HttpWebRequest request)
+        {
+            var timeout = (int) TimeSpan.FromSeconds(15).TotalMilliseconds;
+            request.Timeout = timeout;
+            request.ReadWriteTimeout = timeout;
+        }
+
         [Test]
         public void TestIsUpdateAvailable()
         {
-            var updater = new UpdaterClient();
-            var latest = updater.GetLatestVersionSync();
+            var latest = _client.GetLatestVersionSync();
 
-            Assert.IsTrue(updater.IsUpdateAvailableSync(new Version(0, 0, 0, 0)),
+            Assert.IsTrue(_client.IsUpdateAvailableSync(new Version(0, 0, 0, 0)),
                 "Update SHOULD be available when running an old version");
-            Assert.IsFalse(updater.IsUpdateAvailableSync(latest.Version),
+            Assert.IsFalse(_client.IsUpdateAvailableSync(latest.Version),
                 "Update should NOT be available when already running the latest version");
-            Assert.IsFalse(updater.IsUpdateAvailableSync(new Version(99, 99, 99, 99)),
+            Assert.IsFalse(_client.IsUpdateAvailableSync(new Version(99, 99, 99, 99)),
                 "Update should NOT be available when running a newer version than the latest available");
         }
 
         [Test]
         public void TestDownloadIntegrity()
         {
-            var updater = new UpdaterClient();
-            var latest = updater.GetLatestVersionSync();
+            var latest = _client.GetLatestVersionSync();
             Console.WriteLine("Downloading v{0}", latest.Version);
-            var path = updater.DownloadUpdateAsync().Result;
+            var path = _client.DownloadUpdateAsync().Result;
             Console.WriteLine("Successfully downloaded update file to \"{0}\"", path);
         }
     }
