d0acfc9f07f8ac8e9db767a31ef7e7dbbe2831a2
Andrew C. Dvorak <acdvorak@gmail.com>
2013-08-28 05:16:46 -0500

    Only create setup.exe and portable.zip files - don't create sfx and 7z archives

--- Build/Release/package.bat
+++ Build/Release/package.bat
@@ -15,8 +15,8 @@ REM "%ProgramFiles%\BitRock InstallBuilder Enterprise 8.6.0\bin\builder-cli.exe"
 REM Use `sed` to redact the path to and password for the private key file used to digitally sign EXEs
 call %InnoSetup%\iscc "/sCustom=%SignTool%\signtool.exe $p" Build\InnoSetup\setup.iss | %unxutils%\sed "s/\/[df] .*/[REDACTED]/gi"
 
-%SevenZip%\7za a -sfx7z.sfx -r "%SfxPath%" .\Artifacts\Portable\*
-%SevenZip%\7za a -r "%SevenZipPath%" .\Artifacts\Portable\*
+REM %SevenZip%\7za a -sfx7z.sfx -r "%SfxPath%" .\Artifacts\Portable\*
+REM %SevenZip%\7za a -r "%SevenZipPath%" .\Artifacts\Portable\*
 %SevenZip%\7za a -r "%ZipPath%" .\Artifacts\Portable\*
 
 call Build\Release\sign.bat "BDHero Portable (Self Extracting Archive)" "%ProjectUrl%" "%SfxPath%"
--- Build/Release/update.bat
+++ Build/Release/update.bat
@@ -4,4 +4,4 @@ REM %CD% = C:\Projects\BDHero
 
 call Build\Release\tools.bat
 
-Build\Tools\UMGen --mirror "%MirrorUrl%" --windows --setup "%SetupPath%" --sfx "%SfxPath%" --7z "%SevenZipPath%" --zip "%ZipPath%" --output update.json
+Build\Tools\UMGen --mirror "%MirrorUrl%" --windows --setup "%SetupPath%" --zip "%ZipPath%" --output update.json
