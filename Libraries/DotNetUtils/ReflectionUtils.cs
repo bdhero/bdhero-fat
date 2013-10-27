482081c37687720be6720edf32b5d2cfa103fc45
Andrew C. Dvorak <acdvorak@gmail.com>
2013-10-26 21:12:16 -0500

    Include properties in ReflectionUtils.ToString()

--- Libraries/DotNetUtils/ReflectionUtils.cs
+++ Libraries/DotNetUtils/ReflectionUtils.cs
@@ -11,13 +11,18 @@ namespace DotNetUtils
     {
         public static string ToString(Object obj)
         {
-            var fields = obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
-            return string.Join(Environment.NewLine, fields.Select(info => ToString(obj, info)));
+            var fields = obj.GetType()
+                .GetFields(BindingFlags.Instance | BindingFlags.Public)
+                .Select(info => ToStringImpl(info.GetValue(obj), info));
+            var props = obj.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Select(info => ToStringImpl(info.GetValue(obj, new object[0]), info));
+            return string.Join(Environment.NewLine, fields.Concat(props));
         }
 
-        private static string ToString(Object obj, FieldInfo info)
+        private static string ToStringImpl(Object obj, MemberInfo info)
         {
-            var value = (info.GetValue(obj) ?? "null").ToString();
+            var value = (obj ?? "null").ToString();
             var lines = Regex.Split(value, @"[\n\r\f]+");
             if (lines.Count() > 1)
                 value = Environment.NewLine + string.Join(Environment.NewLine, lines.Select(s => "    " + s));
