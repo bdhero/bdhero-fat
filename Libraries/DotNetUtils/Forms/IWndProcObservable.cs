4a8ec5dead96c220fa4d03b9828d6ecdfbe89d13
Andrew C. Dvorak <acdvorak@gmail.com>
2013-10-31 00:19:40 -0500

    Added IWndProcObservable interface to DotNetUtils/Forms

---
+++ Libraries/DotNetUtils/Forms/IWndProcObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DotNetUtils.Forms
+{
+    public interface IWndProcObservable
+    {
+        event WndProcEventHandler WndProcMessage;
+    }
+
+    public delegate void WndProcEventHandler(ref Message m);
+}
