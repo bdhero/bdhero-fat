e842266d59d467a8e7b14ee0d27df41b478a1600
Andrew C. Dvorak <acdvorak@gmail.com>
2013-11-09 17:37:09 -0600

    Minor/ignore - Removed unused `using`s from MediaPanel.cs

--- BDHeroGUI/Components/MediaPanel.cs
+++ BDHeroGUI/Components/MediaPanel.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Windows.Forms;
 using BDHero.JobQueue;
 using BDHeroGUI.Properties;
