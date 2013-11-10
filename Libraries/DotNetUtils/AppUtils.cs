4577569b7ffef6248e8dd1543dd4a7940610e93e
Andrew C. Dvorak <acdvorak@gmail.com>
2013-11-09 21:40:35 -0600

    Refactored and reformatted AppUtils.cs

--- Libraries/DotNetUtils/AppUtils.cs
+++ Libraries/DotNetUtils/AppUtils.cs
@@ -1,52 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 
 namespace DotNetUtils
 {
     /// <summary>
-    /// Static helper class that provides application assembly meta information,
-    /// such as app name, app version, build date, and product name.
+    ///     Static helper class that provides application assembly meta information,
+    ///     such as app name, app version, build date, and product name.
     /// </summary>
     public static class AppUtils
     {
         #region Assembly Attribute Accessors
 
         /// <summary>
-        /// Gets the human-friendly name of the application.
+        ///     Gets the human-friendly name of the application.
         /// </summary>
-        /// <example><code>"BDHero GUI"</code></example>
+        /// <example>
+        ///     <code>"BDHero GUI"</code>
+        /// </example>
         public static string AppName
         {
             get
             {
-                var assembly = AssemblyUtils.AssemblyOrDefault();
-                object[] attributes = assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-                if (attributes.Length > 0)
+                var titleAttribute = GetAttribute<AssemblyTitleAttribute>();
+                if (titleAttribute != null && !string.IsNullOrWhiteSpace(titleAttribute.Title))
                 {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    if (!string.IsNullOrWhiteSpace(titleAttribute.Title))
-                    {
-                        return titleAttribute.Title;
-                    }
+                    return titleAttribute.Title;
                 }
-                return System.IO.Path.GetFileNameWithoutExtension(assembly.CodeBase);
+                return Path.GetFileNameWithoutExtension(AssemblyUtils.AssemblyOrDefault().CodeBase);
             }
         }
 
         /// <summary>
-        /// Gets the internal name of the application assembly.
+        ///     Gets the internal name of the application assembly.
         /// </summary>
-        /// <example><code>"bdhero-gui"</code></example>
+        /// <example>
+        ///     <code>"bdhero-gui"</code>
+        /// </example>
         public static string AssemblyName
         {
             get { return AssemblyUtils.GetAssemblyName(); }
         }
 
         /// <summary>
-        /// Gets the application's version number.
+        ///     Gets the application's version number.
         /// </summary>
         public static Version AppVersion
         {
@@ -54,79 +53,105 @@ namespace DotNetUtils
         }
 
         /// <summary>
-        /// Gets the human-friendly description of the application.
+        ///     Gets the human-friendly description of the application.
         /// </summary>
-        /// <example><code>"BDHero graphical interface"</code></example>
+        /// <example>
+        ///     <code>"BDHero graphical interface"</code>
+        /// </example>
         public static string AppDescription
         {
-            get
-            {
-                object[] attributes = AssemblyUtils.AssemblyOrDefault().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-            }
+            get { return GetAttributeValue<AssemblyDescriptionAttribute>(attr => attr.Description); }
         }
 
         /// <summary>
-        /// Gets the product name of the application.
+        ///     Gets the product name of the application.
         /// </summary>
-        /// <example><code>"BDHero"</code></example>
+        /// <example>
+        ///     <code>"BDHero"</code>
+        /// </example>
         public static string ProductName
         {
-            get
-            {
-                object[] attributes = AssemblyUtils.AssemblyOrDefault().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
-            }
+            get { return GetAttributeValue<AssemblyProductAttribute>(attr => attr.Product); }
         }
 
         /// <summary>
-        /// Gets the application's copyright string.
+        ///     Gets the application's copyright string.
         /// </summary>
-        /// <example><code>"Copyright © 2013"</code></example>
+        /// <example>
+        ///     <code>"Copyright © 2013"</code>
+        /// </example>
         public static string Copyright
         {
-            get
-            {
-                object[] attributes = AssemblyUtils.AssemblyOrDefault().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-            }
+            get { return GetAttributeValue<AssemblyCopyrightAttribute>(attr => attr.Copyright); }
         }
 
         /// <summary>
-        /// Gets the name of the company that developed the application.
+        ///     Gets the name of the company that developed the application.
         /// </summary>
-        /// <example><code>"BDHero"</code></example>
+        /// <example>
+        ///     <code>"BDHero"</code>
+        /// </example>
         public static string Company
         {
-            get
-            {
-                object[] attributes = AssemblyUtils.AssemblyOrDefault().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
-            }
+            get { return GetAttributeValue<AssemblyCompanyAttribute>(attr => attr.Company); }
         }
 
         /// <summary>
-        /// Gets the date and time the application was built.
+        ///     Gets the date and time the application was built.
         /// </summary>
         public static DateTime BuildDate
         {
             get { return AssemblyUtils.GetLinkerTimestamp(); }
+        }
+
+        /// <summary>
+        ///     Gets the first assembly attribute of type <typeparamref name="T" /> in the entry assembly.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     Type of the assembly attribute to get.
+        ///     E.G., <see cref="AssemblyTitleAttribute" />, <see cref="AssemblyDescriptionAttribute" />.
+        /// </typeparam>
+        /// <returns>
+        ///     The first assembly attribute of type <typeparamref name="T" /> in the entry assembly, or <c>null</c>
+        ///     if the entry assembly does not contain an attribute of type <typeparamref name="T" />.
+        /// </returns>
+        /// <example>
+        ///     <code>
+        /// AssemblyTitleAttribute titleAttr = GetAttribute&lt;AssemblyTitleAttribute&gt;();
+        /// </code>
+        /// </example>
+        private static T GetAttribute<T>() where T : Attribute
+        {
+            return AssemblyUtils.AssemblyOrDefault().GetCustomAttributes(typeof (T), false).FirstOrDefault() as T;
+        }
+
+        /// <summary>
+        ///     Gets the <b>value</b> of the first assembly attribute of type <typeparamref name="T" /> in the
+        ///     entry assembly by evaluating the given <paramref name="expr" /> against the attribute (if it exists).
+        /// </summary>
+        /// <typeparam name="T">
+        ///     Type of the assembly attribute to get.
+        ///     E.G., <see cref="AssemblyTitleAttribute" />, <see cref="AssemblyDescriptionAttribute" />.
+        /// </typeparam>
+        /// <param name="expr">
+        ///     An expression that takes the first entry assembly attribute of type <typeparamref name="T" />
+        ///     and returns a <see cref="String"/> containing its value.  The argument passed to
+        ///     <paramref name="expr"/> will <b>never</b> be null.
+        /// </param>
+        /// <returns>
+        ///     The value of the first assembly attribute of type <typeparamref name="T" /> in the entry assembly,
+        ///     or an empty string (<c>""</c>) if the entry assembly does not contain an attribute of type
+        ///     <typeparamref name="T" />.
+        /// </returns>
+        /// <example>
+        ///     <code>
+        /// AssemblyTitleAttribute titleAttr = GetAttribute&lt;AssemblyTitleAttribute&gt;();
+        /// </code>
+        /// </example>
+        private static string GetAttributeValue<T>(Expression<Func<T, string>> expr) where T : Attribute
+        {
+            var attr = GetAttribute<T>();
+            return attr != null ? expr.Compile().Invoke(attr) : "";
         }
 
         #endregion
